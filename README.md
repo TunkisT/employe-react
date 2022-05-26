7d0aa8cca7f4b0cb8a0afb91f8f42e56c8643659
Tautvydas Tunkis <ttunkis@gmail.com>
2022-05-26 16:18:57 +0300

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+## install
+
+1. npm i
+2. copy .env example to .env and enter your API address
+
+## how to launch web application
+
+1. npm start
--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Register from './pages/Register';
 import AuthContext from './store/authContext';
 
 function App() {
-  const [isLogged, setIsLogged] = useState(true);
+  const [isLogged, setIsLogged] = useState(false);
 
   function login() {
     setIsLogged(true);
