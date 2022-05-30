9452ce5b2e0f59053bfec9cc0c9e4ccaae15ca56
Tautvydas Tunkis <ttunkis@gmail.com>
2022-05-30 11:56:40 +0300

    style changes

--- src/components/Card/Card.style.jsx
+++ src/components/Card/Card.style.jsx
@@ -6,11 +6,11 @@ export const Card = styled.div`
   gap: 1rem;
   margin: 1rem 0;
   min-width: 20rem;
-  padding: 1rem;
+  padding: 0.5rem 0;
+  width: 100%;
   @media screen and (max-width: 768px) {
     overflow: hidden;
     text-align: center;
-    width: 95%;
   }
 `;
 export const Line = styled.p`
